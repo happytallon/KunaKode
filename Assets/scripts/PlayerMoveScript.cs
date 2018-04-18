eaccb9db7adc07123ed4f015196b2b6218f792ec
happytallon <srmarcovich@hotmail.com>
2018-04-17 22:14:20 -0300

    Shoot

--- Assets/scripts/PlayerMoveScript.cs
+++ Assets/scripts/PlayerMoveScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMoveScript : MonoBehaviour
 {	
 	public GameObject hook;
+	public int shootForce;
 	private Rigidbody2D rigidbody;	
 	void Start () {
 		rigidbody = GetComponent<Rigidbody2D>();
@@ -13,16 +14,15 @@ public class PlayerMoveScript : MonoBehaviour
 	void Update () {
 		var x = Input.GetAxis("Horizontal");
 		rigidbody.velocity = new Vector2(x, 0) * 5;
-	}
 
-	void OnMouseButtonDown()
-	{
-		Debug.Log("111");
-		if(GameObject.Find("Hook") != null) return;
+		if(Input.GetButtonDown("Fire1")){
+			Debug.Log("111");
+			if(GameObject.Find("Hook") != null) return;
 
-		Vector2 direction = Input.mousePosition - transform.position;
- 		direction.Normalize();
- 		GameObject projectile = Instantiate( hook, transform.position, Quaternion.identity);
- 		projectile.GetComponent<Rigidbody2D>().velocity = direction * 5;
+			Vector2 direction = Input.mousePosition - transform.position;
+ 			direction.Normalize();
+ 			GameObject projectile = Instantiate( hook, transform.position, Quaternion.identity);
+ 			projectile.GetComponent<Rigidbody2D>().velocity = direction * shootForce;
+		}
 	}
 }
